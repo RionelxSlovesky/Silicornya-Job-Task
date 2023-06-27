320fe791ecddd62de8240da049d7d6622057c8cd
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-06-27 14:36:09 +0600

    Displaying Chart Data
    Displaying chart data dynamically with react recharts

--- src/pages/Student/StudentDashboard.jsx
+++ src/pages/Student/StudentDashboard.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import DashboardCards from "../../components/DashboardCards";
 import { AuthContext } from "../../providers/AuthProvider";
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
 
 
 const StudentDashboard = () => {
@@ -10,6 +11,8 @@ const StudentDashboard = () => {
     const [monthly, setMonthly] = useState([])
     const [yearly, setYearly] = useState([])
     const [weekly, setWeekly] = useState([])
+
+    const [chartOption, setChartOption] = useState('weekly')
 
 
     // fetching dashboard data
@@ -51,7 +54,7 @@ const StudentDashboard = () => {
                     "content-type": "application/json",
                     authorization: `Bearer ${userInfo?.token}`
                 },
-                body: JSON.stringify({"time_period": "monthly"})
+                body: JSON.stringify({ "time_period": "monthly" })
             })
                 .then(res => res.json())
                 .then(data => {
@@ -62,14 +65,14 @@ const StudentDashboard = () => {
                 .catch(err => console.log(err))
 
             //yearly
-            
+
             fetch('http://18.136.192.25:5000/api/v1/dashboard/chart', {
                 method: 'POST',
                 headers: {
                     "content-type": "application/json",
                     authorization: `Bearer ${userInfo?.token}`
                 },
-                body: JSON.stringify({"time_period": "yearly"})
+                body: JSON.stringify({ "time_period": "yearly" })
             })
                 .then(res => res.json())
                 .then(data => {
@@ -87,7 +90,7 @@ const StudentDashboard = () => {
                     "content-type": "application/json",
                     authorization: `Bearer ${userInfo?.token}`
                 },
-                body: JSON.stringify({"time_period": "weekly"})
+                body: JSON.stringify({ "time_period": "weekly" })
             })
                 .then(res => res.json())
                 .then(data => {
@@ -106,7 +109,40 @@ const StudentDashboard = () => {
         <div className="p-4 lg:px-12">
             <DashboardCards dashboardData={dashboardData}></DashboardCards>
             <div className="p-8 shadow-lg rounded-lg">
-                <h1>Your Daily Progress</h1>
+                <h1 className="font-semibold mb-2 text-2xl">Your Daily Progress</h1>
+                <div className="mb-16">
+                    <button onClick={() => setChartOption('weekly')} className="bg-indigo-500 p-2 rounded-lg text-white">Weekly</button>
+                    <button onClick={() => setChartOption('monthly')} className="bg-indigo-500 p-2 md:mx-2 rounded-lg text-white">Monthly</button>
+                    <button onClick={() => setChartOption('yearly')} className="bg-indigo-500 p-2 rounded-lg text-white">Yearly</button>
+                </div>
+
+                {chartOption === 'weekly' &&
+                    <LineChart className="mx-auto" width={600} height={400} data={weekly}>
+                        <Line type="monotone" dataKey="watchHours" stroke="#8884d8" />
+                        <XAxis dataKey="dayName" />
+                        <YAxis />
+                    </LineChart>
+                }
+
+                {chartOption === 'monthly' &&
+                    <LineChart className="mx-auto" width={600} height={400} data={monthly}>
+                        <Line type="monotone" dataKey="watchHours" stroke="#8884d8" />
+                        <XAxis dataKey="dayName" />
+                        <YAxis />
+                    </LineChart>
+                }
+
+                {chartOption === 'yearly' &&
+                    <LineChart className="mx-auto" width={600} height={400} data={yearly}>
+                        <Line type="monotone" dataKey="watchHours" stroke="#8884d8" />
+                        <XAxis dataKey="dayName" />
+                        <YAxis />
+                    </LineChart>
+                }
+
+
+
+
             </div>
         </div>
     );
